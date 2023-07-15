79c8ab8011dd8f51e9de0ec68c5048b434fd8f43
Eduardo Wagner <eduardoodelonw@gmail.com>
2023-07-15 15:47:35 -0300

    refactor(services): switch imports logic to barrel files

---
+++ src/services/api.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://f9f5-45-176-69-251.ngrok-free.app",
+});
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,2 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://f9f5-45-176-69-251.ngrok-free.app",
-});
-
-export default api;
+export * from "./auth";
+export * from "./api";
