6b6e4ab1b75e5497b43e961b8e714b364cf8a5c1
Eduardo Wagner <eduardoodelonw@gmail.com>
2023-07-15 16:22:28 -0300

    Revert "refactor(services): switch imports logic to barrel files"
    This reverts commit 79c8ab8011dd8f51e9de0ec68c5048b434fd8f43.

--- src/services/api.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "https://f9f5-45-176-69-251.ngrok-free.app",
-});
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,7 @@
-export * from "./auth";
-export * from "./api";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://f9f5-45-176-69-251.ngrok-free.app",
+});
+
+export default api;
